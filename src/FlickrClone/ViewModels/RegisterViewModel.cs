c3473d49c7b02062fb91040adc0a07953d88f43a
Andrew Cho and Chris Kuiper <cho-andrew@hotmail.com>
2016-04-27 09:20:38 -0700

    add register functionality

---
+++ src/FlickrClone/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlickrClone.ViewModels
+{
+    public class RegisterViewModel
+    {
+        //UserName is an experiment, not working take out!
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
