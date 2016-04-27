e0318cfe68a403de1500254971d9232895b9d1f3
Andrew Cho and Chris Kuiper <cho-andrew@hotmail.com>
2016-04-27 14:04:27 -0700

    pass second test for comment

---
+++ FlickrClone.Tests/CommentTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlickrClone.Models;
+using Xunit;
+
+namespace FlickrClone.Tests
+{
+    public class CommentTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            var comment = new Comment();
+            comment.Description = "Picture of anything";
+            var result = comment.Description;
+            Assert.Equal("Picture of anything", result);
+        }
+    }
+}
+
