7407ce95f24241cf6b6e65f38763dbae345f2a68
Andrew Cho and Chris Kuiper <cho-andrew@hotmail.com>
2016-04-27 11:37:43 -0700

    add models for images and comments, set up tests

---
+++ FlickrClone.Tests/ImageTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlickrClone.Models;
+using Xunit;
+using FlickrClone.Models;
+
+namespace FlickrClone.Tests
+{
+    public class ImageTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            var image = new Image();
+            var result = image.Description;
+            Assert.Equal("Picture of anything", result);
+        }
+    }
+}
---
+++ src/FlickrClone/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlickrClone.Models
+{
+    [Table("Images")]
+    public class Image
+    {
+        [Key]
+        public int ImageId { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
